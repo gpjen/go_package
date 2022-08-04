b39aefa5b12b5d6c12dfe5d06f636e776de653b7
gpjen95 <nyonggpj9995@gmail.com>
2022-08-04 09:26:21 +0900

    init

---
+++ app.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"go_package/helper"
+	"go_package/other"
+)
+
+func main() {
+	helper.SayHello("gandi jen")
+	other.SayHi("programer")
+}
---
+++ other/other.go
@@ -0,0 +1,7 @@
+package other
+
+import "fmt"
+
+func SayHi(name string) {
+	fmt.Println("other say : Hi,", name)
+}
